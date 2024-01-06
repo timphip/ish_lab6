f83326b6082fe1ebb3720ecc459ccc0d343f73c1
timphip <146890276+timphip@users.noreply.github.com>
2024-01-06 12:04:57 -0400

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+  int DayOfWeek = 0;
+  int daysThatPass = 13;
+  int daysInWeek = 7;
+
+  // write here
+
+
+
+  printf("You started on the 1st (0) day of the week, went %d days from this, so it is now the %d day of the week\n", daysThatPass, endingDayOfWeek);
+}
---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  
+  // variable
+  int n = 13;
+  int m = 10;
+
+  // write here
+
+
+  // print
+  printf("m = %d\n", m);
+  printf("n = %d\n", n);
+}
